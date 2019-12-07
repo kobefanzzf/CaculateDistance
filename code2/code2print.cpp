abe4da10243527621d7d5b53d967ece537006ea4
bugaosuni59 <598586484@qq.com>
2019-12-07 23:36:28 +0800

    no message

--- code2/code2print.cpp
+++ code2/code2print.cpp
@@ -106,7 +106,7 @@ double calc(int root1,int root2,int dep){
 	nmsg+=n;
 	for(int i=0;i<n;i++)msg[rootmsg].push_back({rootmsg+i+1,res[i],dep});
 	for(int i=0;i<n;i++){
-		printf("%d %d %f\n",root1,i>=floor1.size()?-1:floor1[i],res[i]);
+		printf("%d %d %f\n",root1,i>=floor1.size()?-1:floor1[i],abs(res[i]));
 //		printf("%d %d %d %f\n",dep,root1,i>-floor1.size()?-1:floor1[i],res[i]);
 	}
 	for(int i=0;i<n;i++){
